36c982bcccf54ca4f99f9c1310c84f18c6be64b5
sasha-alohin <sashaalohin@ukr.net>
2017-09-20 12:21:46 +0300

    changed controller put

--- src/main/java/com/softbistro/reactive_sample/controllers/CommonPersonController.java
+++ src/main/java/com/softbistro/reactive_sample/controllers/CommonPersonController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -42,7 +43,7 @@ public class CommonPersonController {
 		personService.deletePersonById(id);
 	}
 
-	@PostMapping("/update")
+	@PutMapping("/update")
 	public Person updatePerson(@RequestBody Person person) {
 		return personService.updatePerson(person);
 	}
--- src/main/java/com/softbistro/reactive_sample/controllers/ReactivePersonController.java
+++ src/main/java/com/softbistro/reactive_sample/controllers/ReactivePersonController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -45,7 +46,7 @@ public class ReactivePersonController {
 		return personService.deletePersonById(id);
 	}
 
-	@PostMapping("/update")
+	@PutMapping("/update")
 	public Mono<Person> updatePerson(@RequestBody Person person) {
 		return personService.updatePerson(person);
 	}
